412632779ca4267aa74edb162040429f1329e7ba
ebonutto <elio.bonutto@gmail.com>
2026-09-25 11:59:21 +0200

    fix: overflow check

--- src/elf/64/parse.c
+++ src/elf/64/parse.c
@@ -11,9 +11,12 @@ int parse_elf64(t_file *file, t_elf64 *elf64)
 
 	if (elf64->ehdr->e_phnum == 0)
 		return (1);
-	if (elf64->ehdr->e_phoff + (uint64_t)elf64->ehdr->e_phnum * sizeof(Elf64_Phdr) > file->size)
+	if (elf64->ehdr->e_phoff > file->size)
+		return (1);
+	if (elf64->ehdr->e_phnum * sizeof(Elf64_Phdr) > file->size - elf64->ehdr->e_phoff)
 		return (1);
 
 	elf64->phdr = (Elf64_Phdr *)(file->map + elf64->ehdr->e_phoff);
+
 	return (0);
 }
