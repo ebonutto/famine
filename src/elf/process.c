c670143d12722a14558a8a4e2708e50b3d31b84c
ebonutto <elio.bonutto@gmail.com>
2026-09-25 11:59:41 +0200

    fix: norm

--- src/elf/process.c
+++ src/elf/process.c
@@ -1,13 +1,19 @@
 #include "famine.h"
 
+#include <elf.h> // EI_CLASS
+
 void process_elf(t_file *file)
 {
+	unsigned char *ident;
+
 	if (!is_valid_elf(file))
 		return ;
 
-	if (file->map[EI_CLASS] == ELFCLASS64)
+	ident = file->map;
+
+	if (ident[EI_CLASS] == ELFCLASS64)
 		process_elf64(file);
 
-	// else if (file->map[EI_CLASS] == ELFCLASS32)
+	// else if (ident[EI_CLASS] == ELFCLASS32)
 	// 	process_elf32(file);
 }
