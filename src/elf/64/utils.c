3ccb296e78ff2cff5eb3e77aa3514cb39bbebe16
ebonutto <elio.bonutto@gmail.com>
2026-09-25 14:08:19 +0200

    new: utils.c

---
+++ src/elf/64/utils.c
@@ -0,0 +1,21 @@
+#include "famine.h"
+
+#include <stdint.h> // uintX_t
+
+uint64_t elf64_find_next_segment(Elf64_Phdr *phdr, uint32_t phnum, uint32_t index)
+{
+	uint64_t curr, next, tmp;
+
+	curr = phdr[index].p_offset;
+	next = UINT64_MAX;
+
+	for (uint32_t i = 0; i < phnum; i++) {
+		if (i == index)
+			continue ;
+
+		tmp = phdr[i].p_offset;
+		if (tmp > curr && tmp < next)
+			next = tmp;
+	}
+	return (next);
+}
